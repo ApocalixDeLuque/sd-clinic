3e283ebba4733752d9d72a63eb6fc5e20ee77230
DevSlashRichie <me@slashrichie.dev>
2024-04-03 16:00:08 -0600

    feat: added routes

--- src/app/reports/[slug]/page.tsx
+++ src/app/reports/[slug]/page.tsx
@@ -1,6 +1,17 @@
-import Navbar from '@/app/components/Navbar';
+"use client";
+import { useClient } from "@/api/context";
+import Navbar from "@/app/components/Navbar";
+import { useSearchParams } from "next/navigation";
 
 export default function Page({ params }: { params: { slug: string } }) {
+  const query = useSearchParams();
+
+  const { client } = useClient(query.has("secret") ? "public" : "user");
+
+  const { data } = client.reports.useSwr((f) =>
+    f.one(params.slug, query.get("secret") ?? undefined),
+  )();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-white text-black">
       {/* Navbar */}
